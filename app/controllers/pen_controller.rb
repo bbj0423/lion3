4ea039e8093ad7e76a5a308d6a2ea5b435cbb5f6
Yoon seo Jang <bbj0423@naver.com>
2017-03-28 11:46:30 +0000

    lion3

---
+++ app/controllers/pen_controller.rb
@@ -0,0 +1,14 @@
+class PenController < ApplicationController
+    def IU
+        @a="안녕하세요"
+        @bad =["연애운","돈운","식운","기운"]
+        @p = @bad.sample
+        
+        
+        
+    end
+    def IIIIIU
+    @b="s"
+    
+    end 
+end
