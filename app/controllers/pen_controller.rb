40cb08b5e1f356b53306bd661b10234935adc54c
Yoon seo Jang <bbj0423@naver.com>
2017-03-28 12:17:29 +0000

    lion3

--- app/controllers/pen_controller.rb
+++ app/controllers/pen_controller.rb
@@ -1,8 +1,7 @@
 class PenController < ApplicationController
     def IU
-        @a="안녕하세요"
         @bad =["연애운","돈운","식운","기운"]
-        @p = @bad.sample
+        @A = @bad.sample
         
         
         
