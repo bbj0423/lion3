99fb96d679a8c2cd5b9e0bb7b8586689458f24a6
Yoon seo Jang <bbj0423@naver.com>
2017-04-02 13:46:32 +0000

    first init

--- app/controllers/pen_controller.rb
+++ app/controllers/pen_controller.rb
@@ -1,13 +1,27 @@
 class PenController < ApplicationController
     def IU
-        @bad =["연애운","돈운","식운","기운"]
-        @A = @bad.sample
+        @first =["연애운","금전운","식운","행운"]
+        @A = @first.sample
+        @second=['잘생김','못생김','상여자','상남자']
+        @B = @second.sample
+        @third=['섹시함','햄최4','유연미','코딩력']
+        @C = @third.sample
+        @forth=['유머감','착함','호탕함','세심함']
+        @D= @forth.sample
         
-        
-        
-    end
-    def IIIIIU
-    @b="s"
+        @n1=['1','2','3','4']
+        @m1= @n1.sample
+        @n2=['1','2','3','4']
+        @m2= @n2.sample
+        @n3=['1','2','3','4']
+        @m3= @n3.sample
+        @n4=['1','2','3','4']
+        @m4= @n4.sample
     
-    end 
+    end
+    
+    
+    
+    
+   
 end
